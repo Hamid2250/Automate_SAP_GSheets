41b97d45c8588a6728c6d0ca51623be9df4ed4a9
unknown <pro.designer2250@gmail.com>
2023-01-22 17:56:15 +0300

    WIP update_orders_from_orders_tasks_list

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import setWorkspace
 import re
 import os
+import datetime
+import PyPDF2
 import gspread
 import pyperclip as pc
 from time import sleep
@@ -28,15 +30,15 @@ def sap_response_time():
 def check_items():
     while True:
         if pag.locateOnScreen(image='./images/information.png'):
-            robo.click(image='./images/greenEnter.png')
+            robo.doubleClick(image='./images/greenEnter.png')
             sap_response_time()
         elif pag.locateOnScreen(image='./images/delivery_proposal.png'):
-            robo.click(image='./images/delivery_proposal.png')
+            robo.doubleClick(image='./images/delivery_proposal.png')
             sap_response_time()
         elif pag.locateOnScreen(image='./images/continue.png'):
-            robo.click(image='./images/continue.png')
+            robo.doubleClick(image='./images/continue.png')
             sap_response_time()
-        else:
+        elif pag.locateOnScreen(image='./images/sales_document.png'):
             break
 
 def get_quote_status():
@@ -48,7 +50,7 @@ def get_quote_status():
         return customer_name, customer, q_state
 
 def update_orders_task_list():
-    all_orders_task = orders_task_list.get_all_records()
+    # all_orders_task = orders_task_list.get_all_records()
     quotations = [d["Quotation"] for d in all_orders_task]
     for q in quotations:
         if all_orders_task[quotations.index(q)]["Need Approval"] == "":
@@ -64,6 +66,7 @@ def update_orders_task_list():
                 sap_response_time()
             pag.hotkey('shift', 'f8')
             sap_response_time()
+            robo.click(image='./images/text_with_tabs.png')
             robo.click(image='./images/greenEnter.png')
             sap_response_time()
             pag.hotkey('shift', 'tab')
@@ -84,20 +87,20 @@ def update_orders_task_list():
                 update_task = {'Customer Name': info[0], 'Customer': info[1], 'Need Approval': 'NO',}
                 update_task = dict(current_task, **update_task)
                 update_task = list(update_task.values())
-                orders_task_list.update(str(f'A{current_row}:N{current_row}'), update_task)
+                orders_task_list.batch_update([{'range': f'A{current_row}:N{current_row}', 'values': [update_task]}])
             elif info[2] == 'Q004':
                 update_task = {'Customer Name': info[0], 'Customer': info[1], 'Need Approval': 'YES', 'Approved': 'YES'}
                 update_task = dict(current_task, **update_task)
                 update_task = list(update_task.values())
                 orders_task_list.batch_update([{'range': f'A{current_row}:N{current_row}', 'values': [update_task]}])
-            else:
+            elif info[2] == 'Q002' or info[2] == 'Q003':
                 update_task = {'Customer Name': info[0], 'Customer': info[1], 'Need Approval': 'YES',}
                 update_task = dict(current_task, **update_task)
                 update_task = list(update_task.values())
                 orders_task_list.batch_update([{'range': f'A{current_row}:N{current_row}', 'values': [update_task]}])
 
 def transfer_quotations():
-    all_orders_task = orders_task_list.get_all_records()
+    # all_orders_task = orders_task_list.get_all_records()
     quotations = [d["Quotation"] for d in all_orders_task]
     for q in quotations:
         if all_orders_task[quotations.index(q)]["Finished Date"] == "":
@@ -110,14 +113,15 @@ def transfer_quotations():
                 pag.typewrite(str(q)+'\n')
                 sap_response_time()
                 while True:
-                    if pag.locateOnScreen(image='./images/makkah.png', confidence=0.7) is not None:
+                    if pag.locateOnScreen(image='./images/makkah.png') is not None:
                         sales_office = 'S104'
-                        print(sales_office)
                         break
                 while True:
-                    if pag.locateOnScreen(image='./images/mmwm.png', confidence=0.7) is not None:
+                    if pag.locateOnScreen(image='./images/mmwm.png') is not None:
                         storage_location = 'BN1'
-                        print(storage_location)
+                        break
+                    elif pag.locateOnScreen(image='./images/sm01.png') is not None:
+                        storage_location = 'SM1'
                         break
                 robo.click(image='./images/sales_document.png')
                 robo.click(image='./images/create_subsequent_order.png')
@@ -134,11 +138,11 @@ def transfer_quotations():
                 robo.click(image='./images/sales_document.png')
                 robo.click(image='./images/deliver.png')
                 sap_response_time()
-                if pag.locateOnScreen(image='./images/create_delivery_order.png'):
+                if pag.locateOnScreen(image='./images/create_delivery_with_order.png'):
                     while True:
                         pag.hotkey('enter')
                         sap_response_time
-                        if not pag.locateOnScreen(image='.images/create_delivery_order.png'):
+                        if not pag.locateOnScreen(image='./images/create_delivery_with_order.png'):
                             break
                         else:
                             sleep(1)
@@ -151,12 +155,57 @@ def transfer_quotations():
                 pag.hotkey('ctrl', 'v')
                 robo.click(image='./images/save.png')
                 sap_response_time()
-    
+                current_row = orders_task_list.find(str(q)).row
+                now = datetime.datetime.now()
+                current_task = all_orders_task[quotations.index(q)]
+                update_task = {"Finished Date" : now.strftime("%d/%m/%Y")}
+                update_task = dict(current_task, **update_task)
+                update_task = list(update_task.values())
+                orders_task_list.batch_update([{'range': f'A{current_row}:N{current_row}', 'values': [update_task]}])
 
-# quotations = orders_task_list.get_values('E2:F')
-# status = orders_task_list.get_values('I2:I')
-# new_list = [[x[0], x[1], y[0] if y else ''] for x, y in zip_longest(quotations, status, fillvalue=[''])]
-# print(new_list)
+def pdf_to_txt(f, pdf_file):
+    pdf = PyPDF2.PdfReader(f)
+    with open(pdf_file.replace('.pdf', '.txt'), 'w',encoding='utf-8') as f:
+        for page in range(len(pdf.pages)):
+            text = pdf.pages[page].extract_text()
+            f.write(text)
+
+def update_orders_from_orders_tasks_list():
+    all_orders_task = orders_task_list.get_all_records()
+    quotations = [d["Quotation"] for d in all_orders_task]
+    for q in quotations:
+        if all_orders_task[quotations.index(q)]["Finished Date"] != "":
+            robo.click(image='./images/command_box.png')
+            pag.typewrite('/N VA22\n')
+            sap_response_time()
+            pag.typewrite(str(q))
+            sap_response_time()
+            robo.click(image='./images/document_flow.png')
+            sap_response_time()
+            robo.click(image='./images/print_view.png')
+            sap_response_time()
+            pag.typewrite('LOCAL')
+            robo.click(image='./images/greenEnter.png', offsetUp=60, offsetLeft=10)
+            robo.click(image='./images/immediately.png')
+            robo.click(image='./images/greenEnter.png')
+            sap_response_time()
+            if not pag.locateOnScreen(image='./images/print_to_pdf_selected.png'):
+                robo.click(image='./images/printer_drop_list.png')
+                robo.click(image='./images/print_to_pdf.png')
+            with open('./temp.pdf', 'rb') as f:
+                pdf_to_txt(f, './temp.pdf')            
+
+
+
+# while True:
+#     try:
+#         all_orders_task = orders_task_list.get_all_records()
+#         update_orders_task_list()
+#         transfer_quotations()
+
+#     except gspread.exceptions.APIError:
+#         sleep(60)
+
 
 
 #         pag.hotkey('ctrl', 'c')
@@ -170,10 +219,11 @@ def transfer_quotations():
 #         delivery = re.findall(r'\d+', delivery)
 #         delivery = delivery[0]
 #         print(quotation[0])
-
-# except gspread.exceptions.APIError:
-#     print('No quotation')
     
+
+
+
+
 
 
 
